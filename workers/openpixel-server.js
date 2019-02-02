b9e42db470d816c8699dd993b465cc3a846fb68e
Lukas Mikelionis <lukas@claro.hr>
2019-02-03 00:42:01 +0200

    Implemented NeoPixel server|service

---
+++ workers/openpixel-server.js
@@ -0,0 +1,25 @@
+var ParseStream = require('openpixelcontrol-stream').OpcParseStream;
+var net = require('net');
+var ws281x = require('rpi-ws281x-native');
+
+console.log('Preparations...')
+
+var server = net.createServer(function(conn) {
+
+    console.log('Server was created...');
+
+    var parser = new ParseStream({
+        channel: 1,
+        dataFormat: ParseStream.DataFormat.UINT32_ARRAY
+    });
+
+    parser.on('setpixelcolors', function(data) {
+        console.log('Should process setpixelcolors...');
+        ws281x.render(data);
+    });
+
+    conn.pipe(parser);
+});
+
+ws281x.init(100);
+server.listen(7890);
